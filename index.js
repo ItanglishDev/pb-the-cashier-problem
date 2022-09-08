6b6629e7a483a2c7d7b997048ee155f47d0ed786
chris <chrishethertonweb@gmail.com>
2022-09-08 10:42:25 +0200

    old unuseful code eliminated payment method

--- index.js
+++ index.js
@@ -76,80 +76,43 @@ class TillManager {
         console.log(`${(amount % el).toFixed(0)} ${el} €`);
       }
 
-
-
-
-      // let amountOf100 = (sum % 100).toFixed(0)
-      // if (amountOf100 > 0) {
-      //   leftOver = sum -= amountOf100 * 100
-      //   let amountOf50 = (leftOver % 50).toFixed(0)
-      //   if (amountOf50 > 0) {
-      //     leftOver = sum -= amountOf50 * 50
-      //     let amountOf20 = (leftOver % 20).toFixed(0)
-      //     if (amountOf20 > 0) {
-      //       leftOver = sum -= amountOf20 * 20
-      //       let amountOf10 = (leftOver % 10).toFixed(0)
-      //       if (amountOf20 > 0) {
-      //         leftOver = sum -= amountOf10 * 10
-      //         let amountOf5 = (leftOver % 5).toFixed(0)
-      //         if (amountOf5 > 0) {
-      //           leftOver = sum -= amountOf10 * 10
-      //           let amountOf1 = (leftOver % 1).toFixed(0)
-      //           if (amountOf1 > 0) {
-      //             leftOver = sum -= amountOf1
-      //             let amountOf50Cent = (leftOver % .5).toFixed(0)
-      //             if (amountOf50Cent > 0) {
-      //               leftOver = sum -= amountOf50Cent *.5
-      //               let amountOf20Cent = (leftOver % .2).toFixed(0)
-      //             }
-      //           }
-      //         }
-      //       }
-      //     }
-      //   }
-      // }
-
-
     }
-
-
-}
 
   // NEW SHOP CREATED
   const edeka = new Shop
 
-  // PRODUCT MANAGER CREATED
-  const managingNewProducts = new ProductManager(edeka.products)
+    // PRODUCT MANAGER CREATED
+    const managingNewProducts = new ProductManager(edeka.products)
 
-  // PRODUCT CREATED
-  const banana = new Product('Banana', 1, 345)
-  const pineapple = new Product('Pineapple', 1.50, 245)
-  const apple = new Product('Apple', 0.75, 645)
-  const orange = new Product('Orange', 1.2, 347)
-  const strawberry = new Product('Strawberry', 2.5, 375)
+    // PRODUCT CREATED
+    const banana = new Product('Banana', 1, 345)
+    const pineapple = new Product('Pineapple', 1.50, 245)
+    const apple = new Product('Apple', 0.75, 645)
+    const orange = new Product('Orange', 1.2, 347)
+    const strawberry = new Product('Strawberry', 2.5, 375)
 
-// ADDING PRODUCTS TO SHOP
-managingNewProducts.addToStore(banana)
-managingNewProducts.addToStore(pineapple)
-managingNewProducts.addToStore(apple)
-managingNewProducts.addToStore(orange)
-managingNewProducts.addToStore(strawberry)
+    // ADDING PRODUCTS TO SHOP
+    managingNewProducts.addToStore(banana)
+    managingNewProducts.addToStore(pineapple)
+    managingNewProducts.addToStore(apple)
+    managingNewProducts.addToStore(orange)
+    managingNewProducts.addToStore(strawberry)
 
-  // TILL MANAGER CREATED
-  const cashier = new TillManager(edeka.till)
+    // TILL MANAGER CREATED
+    const cashier = new TillManager(edeka.till)
 
-// ADD PRODUCT TO TILL
-cashier.buyProduct(banana)
-cashier.buyProduct(banana)
-cashier.buyProduct(banana)
-cashier.buyProduct(pineapple)
+    // ADD PRODUCT TO TILL
+    cashier.buyProduct(banana)
+    cashier.buyProduct(banana)
+    cashier.buyProduct(banana)
+    cashier.buyProduct(pineapple)
 
-// console.log("TILL1 ===>", edeka.till);
-// REMOVE PRODUCT FROM TILL
-cashier.removeProduct(pineapple)
-// console.log(pineapple);
-console.log("TILL2 ===>", edeka.till);
+    // console.log("TILL1 ===>", edeka.till);
+    // REMOVE PRODUCT FROM TILL
+    cashier.removeProduct(pineapple)
+    // console.log(pineapple);
+    console.log("TILL2 ===>", edeka.till);
 
-// cashier.paymentMethod('card')
-// console.log(edeka.products);
-console.log(cashier.total());
+    // cashier.paymentMethod('card')
+    // console.log(edeka.products);
+    console.log(cashier.total());
