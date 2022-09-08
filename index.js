3560c6f39abcaf3257e67924c9ab67761c864b79
chris <chrishethertonweb@gmail.com>
2022-09-08 10:50:54 +0200

    corrections made for payment method

--- index.js
+++ index.js
@@ -69,50 +69,52 @@ class TillManager {
     console.log(`The change to be given is ${amount - total()}, please follow the output to give change:  `);
     const currency = [200, 100, 50, 20, 10, 5, 1, .5, .2, .1]
 
-    currency.forEach(el => {
+    currency.forEach(el = () => {
       if ((amount % el).toFixed(0) > 0) {
 
         amount = amount - (amount % el).toFixed(0) * el
-        console.log(`${(amount % el).toFixed(0)} ${el} €`);
+        console.log(`${(amount % el).toFixed(0)} x ${el}€`);
       }
 
-    }
+    })
+  }
+}
 
-  // NEW SHOP CREATED
-  const edeka = new Shop
+// NEW SHOP CREATED
+const edeka = new Shop
 
-    // PRODUCT MANAGER CREATED
-    const managingNewProducts = new ProductManager(edeka.products)
+// PRODUCT MANAGER CREATED
+const managingNewProducts = new ProductManager(edeka.products)
 
-    // PRODUCT CREATED
-    const banana = new Product('Banana', 1, 345)
-    const pineapple = new Product('Pineapple', 1.50, 245)
-    const apple = new Product('Apple', 0.75, 645)
-    const orange = new Product('Orange', 1.2, 347)
-    const strawberry = new Product('Strawberry', 2.5, 375)
+// PRODUCT CREATED
+const banana = new Product('Banana', 1, 345)
+const pineapple = new Product('Pineapple', 1.50, 245)
+const apple = new Product('Apple', 0.75, 645)
+const orange = new Product('Orange', 1.2, 347)
+const strawberry = new Product('Strawberry', 2.5, 375)
 
-    // ADDING PRODUCTS TO SHOP
-    managingNewProducts.addToStore(banana)
-    managingNewProducts.addToStore(pineapple)
-    managingNewProducts.addToStore(apple)
-    managingNewProducts.addToStore(orange)
-    managingNewProducts.addToStore(strawberry)
+// ADDING PRODUCTS TO SHOP
+managingNewProducts.addToStore(banana)
+managingNewProducts.addToStore(pineapple)
+managingNewProducts.addToStore(apple)
+managingNewProducts.addToStore(orange)
+managingNewProducts.addToStore(strawberry)
 
-    // TILL MANAGER CREATED
-    const cashier = new TillManager(edeka.till)
+// TILL MANAGER CREATED
+const cashier = new TillManager(edeka.till)
 
-    // ADD PRODUCT TO TILL
-    cashier.buyProduct(banana)
-    cashier.buyProduct(banana)
-    cashier.buyProduct(banana)
-    cashier.buyProduct(pineapple)
+// ADD PRODUCT TO TILL
+cashier.buyProduct(banana)
+cashier.buyProduct(banana)
+cashier.buyProduct(banana)
+cashier.buyProduct(pineapple)
 
-    // console.log("TILL1 ===>", edeka.till);
-    // REMOVE PRODUCT FROM TILL
-    cashier.removeProduct(pineapple)
-    // console.log(pineapple);
-    console.log("TILL2 ===>", edeka.till);
+// console.log("TILL1 ===>", edeka.till);
+// REMOVE PRODUCT FROM TILL
+cashier.removeProduct(pineapple)
+// console.log(pineapple);
+console.log("TILL2 ===>", edeka.till);
 
-    // cashier.paymentMethod('card')
-    // console.log(edeka.products);
-    console.log(cashier.total());
+// cashier.paymentMethod('card')
+// console.log(edeka.products);
+console.log(cashier.total());
